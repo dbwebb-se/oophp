af6b2f7b36ed4ee6e78e7355bb64db99328850c1
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-03-03 17:03:18 +0100

    adding config for url

--- example/anax-lite/config/url.php
+++ example/anax-lite/config/url.php
@@ -1,16 +1,14 @@
 <?php
 /**
  * Config file for url.
- * The variable $this is here related to $di.
  */
 return [
-
     // Defaults to use when creating urls.
     //"siteUrl"       => null,
     //"baseUrl"       => null,
     //"staticSiteUrl" => null,
     //"staticBaseUrl" => null,
     //"scriptName"    => null,
+    //"urlType"       => self::URL_CLEAN,
     "urlType"       => self::URL_APPEND,
-
 ];
