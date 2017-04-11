6652c5ce7b067a138c5a463efa243e2884ddbadd
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-04-11 16:47:14 +0200

    fix validation

--- example/content/src/function.php
+++ example/content/src/function.php
@@ -77,7 +77,8 @@ function esc($value)
  * 
  * @return str the formatted slug.
  */
-function slugify($str) {
+function slugify($str)
+{
     $str = mb_strtolower(trim($str));
     $str = str_replace(array('å','ä','ö'), array('a','a','o'), $str);
     $str = preg_replace('/[^a-z0-9-]/', '-', $str);
