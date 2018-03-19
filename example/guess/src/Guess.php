985300a95c2d95da3d0c8ecb84bdbb549922aa2c
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-03-20 00:57:09 +0100

    release kmom01

---
+++ example/guess/src/Guess.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Guess my number, a class supporting the game through GET, POST and SESSION.
+ */
+class Guess
+{
+    /**
+     * @var int $number   The current secret number.
+     * @var int $tries    Number of tries a guess has been made.
+     */
+
+
+
+    /**
+     * Constructor to initiate the object with current game settings,
+     * if available. Randomize the number if null is sent in.
+     *
+     * @param int $number The current secret number, default -1 to initiate
+     *                    the number from start.
+     * @param int $tries  Number of tries a guess has been made,
+     *                    default 6.
+     */
+    /*
+    public function __construct(int $number = -1, int $tries = 6)
+    { }
+    */
+
+
+
+    /**
+     * Randomize the secret number between 1 and 100 to initiate a new game.
+     *
+     * @return void
+     */
+    /*
+    public function random()
+    { }
+    */
+
+
+
+    /**
+     * Get number of tries left.
+     *
+     * @return int as number of tries made.
+     */
+    /*
+    public function tries()
+    { }
+    */
+
+
+
+    /**
+     * Get the secret number.
+     *
+     * @return int as the secret number.
+     */
+    /*
+    public function number()
+    { }
+    */
+
+
+
+    /**
+     * Make a guess, decrease remaining guesses and return a string stating
+     * if the guess was correct, too low or to high or if no guesses remains.
+     * 
+     * @throws GuessException when guessed number is out of bounds.
+     *
+     * @return string to show the status of the guess made.
+     */
+    /*
+    public function makeGuess($number)
+    { }
+    */
+}
