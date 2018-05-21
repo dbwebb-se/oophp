279bdeea8b9675542f4b2eee9dec651387e3100a
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-05-21 12:03:30 +0200

    validate slugify using phpmd

--- example/content/src/function.php
+++ example/content/src/function.php
@@ -80,7 +80,8 @@ function esc($value)
 function slugify($str)
 {
     $str = mb_strtolower(trim($str));
-    $str = str_replace(array('å','ä','ö'), array('a','a','o'), $str);
+    $str = str_replace(['å','ä'], 'a', $str);
+    $str = str_replace('ö', 'o', $str);
     $str = preg_replace('/[^a-z0-9-]/', '-', $str);
     $str = trim(preg_replace('/-+/', '-', $str), '-');
     return $str;
