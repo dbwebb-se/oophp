24c9e7bb32adc2eb49a05011b95170c45028b2de
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-04-25 17:00:13 +0200

    inspect kmom02

--- .dbwebb/script/inspect/kmom.d/kmom02/01_npm-install.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd me/kmom02/terminal1 || exit 1
-if [[ -f package.json ]]; then
-    npm install
-fi
--- .dbwebb/script/inspect/kmom.d/kmom03/01_npm-install.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd me/kmom03/terminal2 || exit 1
-if [[ -f package.json ]]; then
-    npm install
-fi
