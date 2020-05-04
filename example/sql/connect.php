0e19d6ad13092e80f429698a9f120175878141eb
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-05-04 15:01:41 +0200

    Show all databases

--- example/sql/connect.php
+++ example/sql/connect.php
@@ -11,8 +11,8 @@ ini_set('display_errors', 'on');
 // ];
 
 $databaseConfig = [
-    "dsn"      => "mysql:host=127.0.0.1;charset=utf8;",
-    //"dsn"      => "mysql:host=127.0.0.1;port=13306;charset=utf8;",
+    //"dsn"      => "mysql:host=127.0.0.1;charset=utf8;",
+    "dsn"      => "mysql:host=127.0.0.1;port=13306;charset=utf8;",
     "login"    => "user",
     "password" => "pass",
     "options"  => null,
@@ -23,5 +23,5 @@ $db = new PDO(...array_values($databaseConfig));
 $sql = "SHOW DATABASES";
 $stm = $db->prepare($sql);
 $stm->execute();
-$res = $stm->fetch();
+$res = $stm->fetchAll();
 var_dump($res);
